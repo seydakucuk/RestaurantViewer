c9254d2a1244fcbe6539984695e852483be9bb64
Şeyda KÜÇÜK <shatipoglu@stm.com.tr>
2019-02-15 09:59:39 +0300

    button role added

--- js/main.js
+++ js/main.js
@@ -181,6 +181,7 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
+  more.setAttribute("role","button");
   li.append(more)
 
   more.setAttribute("tabIndex",tabIndexCounter);
